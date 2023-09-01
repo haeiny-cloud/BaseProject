63f40207521c4bec4bc547e6af2051243fbdc4e8
kyle-dever <kyle.park@devercorp.com>
2023-09-01 11:05:44 +0900

    feat: toolbar로 navigation view open / close 버튼 달기

--- app/src/main/java/com/kyle/luckyfivetest/ui/MainActivity.kt
+++ app/src/main/java/com/kyle/luckyfivetest/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.kyle.luckyfivetest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.kyle.luckyfivetest.R
 import com.kyle.luckyfivetest.databinding.ActivityMainBinding
 import com.kyle.luckyfivetest.ui.base.BaseFragment
@@ -12,7 +18,7 @@ import com.kyle.luckyfivetest.ui.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), BaseFragment.CallBack {
+class MainActivity : AppCompatActivity(), BaseFragment.CallBack, NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -20,6 +26,42 @@ class MainActivity : AppCompatActivity(), BaseFragment.CallBack {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        initToolbar()
+        initMainFragment()
+
+        initDrawerViewAndEvents()
+    }
+
+    private fun initDrawerViewAndEvents() {
+        binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val headerView = binding.navigationView.getHeaderView(0)
+        val btnClose = headerView.findViewById<ImageView>(R.id.btn_close)
+
+        btnClose.setOnClickListener {
+            binding.drawerLayout.closeDrawers()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> { // 메뉴 버튼
+                binding.drawerLayout.openDrawer(GravityCompat.START)    // 네비게이션 드로어 열기
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.content.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 표시 유무
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_action_open_drawer) // 뒤로가기 버튼 이미지 변경
+        supportActionBar?.setDisplayShowTitleEnabled(false) // custom title (false)
+        binding.content.toolbar.title = "test"
+    }
+
+    private fun initMainFragment() {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
 
@@ -41,5 +83,17 @@ class MainActivity : AppCompatActivity(), BaseFragment.CallBack {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item1 -> Toast.makeText(this, "item1", Toast.LENGTH_SHORT).show()
+            R.id.item2 -> Toast.makeText(this, "item2", Toast.LENGTH_SHORT).show()
+            R.id.item3 -> Toast.makeText(this, "item3", Toast.LENGTH_SHORT).show()
+        }
+
+        // close navigation view
+        binding.drawerLayout.closeDrawers()
+        return false
     }
 }
