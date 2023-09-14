347c8f2586d9e2ec1cf43ae588791e4a8c9dc614
kyle-dever <kyle.park@devercorp.com>
2023-09-14 09:02:21 +0900

    fix: 애니메이션 재생 도중 취소버튼 클릭 시 강제종료 오류 수정

--- app/src/main/java/com/kyle/luckyfivetest/ui/boxopen/BoxOpenFragment.kt
+++ app/src/main/java/com/kyle/luckyfivetest/ui/boxopen/BoxOpenFragment.kt
@@ -2,6 +2,7 @@ package com.kyle.luckyfivetest.ui.boxopen
 
 import android.animation.ObjectAnimator
 import android.view.animation.AccelerateInterpolator
+import androidx.core.animation.doOnCancel
 import androidx.core.animation.doOnEnd
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -9,21 +10,29 @@ import com.kyle.luckyfivetest.R
 import com.kyle.luckyfivetest.databinding.FragmentBoxOpenBinding
 import com.kyle.luckyfivetest.ui.base.BaseFragment
 
-class BoxOpenFragment() : BaseFragment<FragmentBoxOpenBinding, BoxOpenViewModel>() {
+class BoxOpenFragment : BaseFragment<FragmentBoxOpenBinding, BoxOpenViewModel>() {
 
     override val layoutResId: Int = R.layout.fragment_box_open
     override val viewModel: BoxOpenViewModel by viewModels()
     override val fragment: String = " "
 
+    private lateinit var rotateAnimation: ObjectAnimator
+    private lateinit var fadeOutAnimation: ObjectAnimator
+
     override fun onCreate() {
-        val rotateAnimation = ObjectAnimator.ofFloat(mViewDataBinding.clover, "rotation", 0f, 3600f)
+        rotateAnimation = ObjectAnimator.ofFloat(mViewDataBinding.clover, "rotation", 0f, 3600f)
         rotateAnimation.apply {
             interpolator = AccelerateInterpolator()
             duration = 3000
-            doOnEnd { findNavController().navigate(R.id.action_boxOpenFragment_to_boxResultFragment) }
+            doOnCancel {
+                removeAllListeners()
+            }
+            doOnEnd {
+                findNavController().navigate(R.id.action_boxOpenFragment_to_boxResultFragment)
+            }
         }
 
-        val fadeOutAnimation = ObjectAnimator.ofFloat(mViewDataBinding.layout, "alpha", 1f, 0f)
+        fadeOutAnimation = ObjectAnimator.ofFloat(mViewDataBinding.layout, "alpha", 1f, 0f)
         fadeOutAnimation.apply {
             interpolator = AccelerateInterpolator()
             duration = 3000
@@ -33,5 +42,10 @@ class BoxOpenFragment() : BaseFragment<FragmentBoxOpenBinding, BoxOpenViewModel>
             rotateAnimation.start()
             fadeOutAnimation.start()
         }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        rotateAnimation.cancel()
     }
 }
